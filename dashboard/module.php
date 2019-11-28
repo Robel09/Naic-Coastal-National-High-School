aa05a4720136af6eada70bbbb94f8613df028343
Rhalp Darren R. Cabrera <rhalpdarrencabrera@gmail.com>
2019-11-28 11:31:22 +0800

    dashboard/module

---
+++ dashboard/module.php
@@ -0,0 +1,497 @@
+<?php 
+include('../session.php');
+
+
+require_once("../class.user.php");
+
+  
+$auth_user = new USER();
+// $page_level = 3;
+// $auth_user->check_accesslevel($page_level);
+$pageTitle = "Manage Module";
+$mod_ID = "";
+if(isset($_REQUEST['mod_ID'])){
+  $mod_ID = $_REQUEST['mod_ID'];
+  $this_room_ID = $_REQUEST['room_ID'];
+
+}
+?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <?php 
+      include('x-meta.php');
+    ?>
+
+
+  <?php 
+  include('x-css.php');
+  ?>
+ 
+
+
+
+    <style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        -ms-user-select: none;
+        user-select: none;
+      }
+
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+    </style>
+    <!-- Custom styles for this template -->
+    <link href="../assets/css/dashboard.css" rel="stylesheet">
+  </head>
+  <body>
+<?php 
+include('x-nav.php');
+?>
+
+<div class="container-fluid">
+  <div class="row">
+      <?php 
+    include('x-sidenav.php');
+    ?>
+
+    <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4">
+      <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+        <h3 class="h4">Manage Module Topics & Subtopic</h3>
+        
+      </div>
+
+      <div class="table-responsive">
+         <button type="button" class="btn btn-sm btn-success add" >
+            Add Topic
+          </button>
+           <button type="button" class="btn btn-sm btn-info float-right material" data-toggle="modal" data-target="#material_modal">Room Files</button>
+         <br><br>
+        <table class="table table-borderless table-sm" id="topic_data">
+          <thead>
+            <tr>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            
+     
+          </tbody>
+        </table>
+
+      </div>
+<!-- Modal -->
+<div class="modal fade" id="m_subtopic" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-lg" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="m_subtopic_title">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body" id="m_subtopic_content">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- Modal -->
+<div class="modal fade" id="m_topic" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-lg" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="m_subtopic_title">Add Topic</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body" id="m_subtopic_content">
+      <form method="post" id="topic_form" enctype="multipart/form-data">
+       <div class="form-row">
+        
+           <div class="form-group col-md-12">
+             <label for="topic_title">Topic<span class="text-danger">*</span></label>
+             <input type="text" class="form-control" id="topic_title" name="topic_title" placeholder="" value=""  required=""  >
+           </div>
+         </div>
+      </div>
+      <div class="modal-footer">
+        
+        <input type="hidden" name="operation" id="operation">
+        <input type="hidden" name="topic_ID" id="topic_ID">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="submit" class="btn btn-secondary" id="submit_t">Submit</button>
+      </div>
+       </form>
+    </div>
+  </div>
+</div>
+
+
+<!-- Modal -->
+<div class="modal fade" id="m_subtopicx" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-lg" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="m_subtopic_title">Add Subtopic</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body" id="m_subtopic_content">
+          <form method="post" id="subtopic_form" enctype="multipart/form-data">
+       <div class="form-row">
+        
+           <div class="form-group col-md-12">
+             <label for="subtopic_title">Topic<span class="text-danger">*</span></label>
+             <input type="text" class="form-control" id="subtopic_title" name="subtopic_title" placeholder="" value=""  required=""  >
+           </div>
+            <div class="form-group col-md-12">
+             <label for="subtopic_content">Content<span class="text-danger">*</span></label>
+             <textarea class="form-control" id="subtopic_content" name="subtopic_content"  value=""  required=""  ></textarea>
+           </div>
+
+         </div>
+      </div>
+      <div class="modal-footer">
+        <input type="hidden" name="operation1" id="operation1">
+        <input type="hidden" name="submtop_ID" id="submtop_ID">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="submit" class="btn btn-secondary" id="submit_tt">Submit</button>
+      </div>
+       </form>
+    </div>
+  </div>
+</div>
+
+<!-- Modal -->
+<div class="modal fade" id="material_modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-lg" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Room Files</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <?php 
+          if($auth_user->student_level()){
+            ?><?php
+          }
+          else{
+            ?><button type="button" class="btn btn-sm btn-success add_materials" >Add Room Files</button><?php
+          }
+
+        ?>
+          
+          <br><br>
+          <table class="table table-striped table-sm" id="material_data">
+           <thead>
+            <tr>
+              <th>#</th>
+              <th width="50%">Name</th>
+              <th>Type</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            
+     
+          </tbody>
+        </table>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+</div>
+</div>
+
+
+    </main>
+  </div>
+</div>
+
+<?php 
+include('x-script.php');
+?>
+        <script type="text/javascript">
+   
+
+
+          $(document).ready(function() {
+             
+            var dataTable = $('#topic_data').DataTable({
+            "processing":true,
+            "serverSide":true,
+            "order":[],
+            "bAutoWidth": false,
+              "searching": false,
+            //   "ordering": false,
+            "info":     false,
+            "paging":     false,
+            "ajax":{
+              url:"datatable/room_module/fetch_topic.php?mod_ID=<?php echo $mod_ID?>",
+              type:"POST"
+            },
+            "columnDefs":[
+              {
+                "targets":[0],
+                "orderable":false,
+              },
+            ],
+
+          });
+
+          $(document).on('submit', '#topic_form', function(event){
+             event.preventDefault();
+    
+             var topic_ID = $("#topic_ID").val();
+             var topic_title = $("#topic_title").val();
+
+              var operation = $('#operation').val();
+             // alert(topic_title);
+                $.ajax({
+                url:"load1.php",
+                method:'POST',
+                data:{action:operation,mod_ID:<?php echo $mod_ID?>,topic_title:topic_title,topic_ID:topic_ID},
+                dataType:"json",
+                complete:function(data)
+                {
+                  
+                 dataTable.ajax.reload();
+                 $('#m_topic').modal("hide");
+                  
+                }
+              });
+           
+          });
+
+          $(document).on('submit', '#subtopic_form', function(event){
+              event.preventDefault();
+    
+            var subtopic_title = $("#subtopic_title").val();
+            var subtopic_content = $("#subtopic_content").val();
+            var topic_ID = $("#topic_ID").val();
+            var submtop_ID = $("#submtop_ID").val();
+            var operation1 = $('#operation1').val();
+             
+
+              $.ajax({
+                url:"load1.php",
+                method:'POST',
+                data:{
+                  action:operation1,
+                  mod_ID:<?php echo $mod_ID?>,
+                  topic_ID:topic_ID,
+                  submtop_ID:submtop_ID,
+                  subtopic_title:subtopic_title,
+                  subtopic_content:subtopic_content},
+                dataType:"json",
+                complete:function(data)
+                {
+                  
+                 dataTable.ajax.reload();
+                  $('#m_subtopicx').modal("hide");
+                 
+                  
+                }
+              });
+           
+          });
+
+
+
+
+            $(document).on('click', '.add', function(){
+            $('#m_topic').modal("show");
+            $('#operation').val("add_topic");
+            $('#submit_t').text("Submit");
+              
+             $('#topic_form')[0].reset();
+              
+            }); 
+   
+
+             $(document).on('click', '.edit_topic', function(){
+            
+             var topic_ID = $(this).attr("id");
+
+            $('#operation').val("update_topic");
+            $('#submit_t').text("Update");
+            $('#topic_ID').val(topic_ID);
+
+                $.ajax({
+                url:"load1.php",
+                method:'POST',
+                data:{action:"get_topic",topic_ID:topic_ID},
+                // contentType:false,
+                // processData:false,   
+                dataType:"json",
+                success:function(data)
+                {
+                  
+                    $('#m_topic').modal("show");
+                    $("#topic_ID").val(data.mtopic_ID);
+                    $("#topic_title").val(data.mtopic_Title);
+                    dataTable.ajax.reload();
+                }
+              });
+             
+              
+              
+            }); 
+
+            $(document).on('click', '.delete_topic', function(){
+            
+             var topic_ID = $(this).attr("id");
+
+             if (confirm("Are you sure you want to delete this topic?")) {
+                   $.ajax({
+                  url:"load1.php",
+                  method:'POST',
+                  data:{action:"delete_topic",topic_ID:topic_ID},
+                  dataType:"json",
+                  complete:function(data)
+                  {
+                    dataTable.ajax.reload();
+                    
+                    
+                  }
+                });
+              } else {
+                  return false;
+              }
+             
+              
+              
+            });   
+
+              
+
+            $(document).on('click', '.add_subtopic', function(){
+              var topicID = $(this).attr("id");
+              // alert(subtopicID);
+               $("#topic_ID").val(topicID);
+               $('#m_subtopicx').modal("show");
+
+             $('#subtopic_form')[0].reset();
+               
+
+              $('#operation1').val("add_subtopic");
+              $('#submit_tt').text("Submit");
+
+            });
+
+            $(document).on('click', '.view_subtopic', function(){
+            var subtopic_ID = $(this).attr("sub-topic");
+      
+              
+              $('#m_subtopic').modal("show");
+                $.ajax({
+                url:"load1.php",
+                method:'POST',
+                data:{action:"get_subtopic",submtop_ID:subtopic_ID},
+                dataType:"json",
+                success:function(data)
+                {
+                  $("#m_subtopic_title").html(data.submtop_Title);
+                  $("#m_subtopic_content").html(data.submtop_Content);
+                  
+                  
+                }
+              });
+            });
+
+             $(document).on('click', '.edit_subtopic', function(){
+            var subtopicID = $(this).attr("sub-topic");
+              // alert("Edit:"+subtopicID);
+
+               $('#m_subtopicx').modal("show");
+            $('#operation1').val("update_subtopic");
+            $('#submit_tt').text("Update");
+            $('#submtop_ID').val(subtopicID);
+
+                $.ajax({
+                url:"load1.php",
+                method:'POST',
+                data:{action:"get_subtopic",submtop_ID:subtopicID},
+                dataType:"json",
+                success:function(data)
+                {
+                  $("#subtopic_title").val(data.submtop_Title);
+                  $("#subtopic_content").val(data.submtop_Content);
+                  
+                  
+                }
+              });
+             
+              
+
+
+            });
+              $(document).on('click', '.delete_subtopic', function(){
+            var subtopicID = $(this).attr("sub-topic");
+       
+
+
+             if (confirm("Are you sure you want to delete this subtopic?")) {
+                  $.ajax({
+                  url:"load1.php",
+                  method:'POST',
+                  data:{action:"delete_subtopic",subtopicID:subtopicID},
+                  dataType:"json",
+                  complete:function(data)
+                  {
+                    dataTable.ajax.reload();
+                    
+                    
+                  }
+                });
+              } else {
+                  return false;
+              }
+             
+            });
+
+          //************MATERIAL SCRIPT************//
+
+          var materials_dataTable = $('#material_data').DataTable({
+            "processing":true,
+            "serverSide":true,
+            "bAutoWidth": false,
+            "order":[],
+            "ajax":{
+              url:"datatable/room_materials/fetch.php?room_ID="+<?php echo $this_room_ID?>+"&mod_ID="+<?php echo $mod_ID?>,
+              type:"POST"
+            },
+            "columnDefs":[
+              {
+                "targets":[0],
+                "orderable":false,
+              },
+            ],
+
+          });
+
+          
+          } );
+
+
+        </script>
+        </body>
+
+</html>
